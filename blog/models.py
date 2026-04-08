b2acb7dab825e6dc5cbbc0205db1ec562712501e
donghun <donghunjeong01@gmail.com>
2026-04-09 01:22:27 +0900

    시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
 # Create your models here.
