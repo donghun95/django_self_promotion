35c02f0dc34947cf87d8f505543ca8b93e27b33b
donghun <donghunjeong01@gmail.com>
2026-04-13 00:21:59 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- django_self_promotion/settings.py
+++ django_self_promotion/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_URL
+
+STATIC_URL = '/static/'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,5 +120,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
